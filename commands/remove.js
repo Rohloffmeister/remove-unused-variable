4e4a171499a36498079560cd184ddf0d8a297cac
Rohloffmeister <132856636+Rohloffmeister@users.noreply.github.com>
2024-11-04 16:07:35 +0100

    Update remove.js
    added alert for no unused variables found

--- commands/remove.js
+++ commands/remove.js
@@ -5,6 +5,7 @@ const traverse = require("@babel/traverse").default;
 const generate = require("@babel/generator").default;
 const { isValidDocumentLanguage } = require("../utils.js");
 
+
 module.exports = function () {
   const { activeTextEditor } = vscode.window;
   if (!activeTextEditor) {
@@ -21,51 +22,53 @@ module.exports = function () {
     sourceType: "module",
     // parse in strict mode and allow module declarations
     plugins: [
-      // enable jsx and flow syntax
-      "jsx",
-      "flow",
-      "decorators-legacy",
-      "decoratorsBeforeExport"
-    ],
-  });
+    // enable jsx and flow syntax
+    "jsx",
+    "flow",
+    "decorators-legacy",
+    "decoratorsBeforeExport"] });
 
+
+
+  let unusedVariablesFound = false;
 
   // 处理函数参数
   function getParamsList(path) {
     const { node } = path;
     const { params = [] } = node;
-    let paramsList = [...params]
+    let paramsList = [...params];
     for (let i = params.length - 1; i >= 0; i--) {
       let paramsId = params[i];
-      if (paramsId && paramsId.type === 'ObjectPattern') { // 如果参数是以对象形式的解构 {}
-        if (!paramsId.properties || !paramsId.properties.length) continue;  // 如果对象中参数为空
+      if (paramsId && paramsId.type === 'ObjectPattern') {// 如果参数是以对象形式的解构 {}
+        if (!paramsId.properties || !paramsId.properties.length) continue; // 如果对象中参数为空
         let propertiesList = [...paramsId.properties];
 
-        paramsId.properties.forEach(k => {
+        paramsId.properties.forEach((k) => {
           const paramsBinding = path.scope.getBinding(k.key.name);
-          if (!paramsBinding.referenced) { // 如果没被引用
-            propertiesList = propertiesList.filter(w => w.key.name !== k.key.name)
+          if (!paramsBinding.referenced) {// 如果没被引用
+            propertiesList = propertiesList.filter((w) => w.key.name !== k.key.name);
           }
-        })
-        if (!propertiesList.length) { // 如果对象中参数都没有引用 去除当前的解构对象
-          paramsList.pop()
+        });
+        if (!propertiesList.length) {// 如果对象中参数都没有引用 去除当前的解构对象
+          paramsList.pop();
         }
-        paramsId.properties = propertiesList
+        paramsId.properties = propertiesList;
 
       } else {
         const paramsBinding = path.scope.getBinding(paramsId.name);
         if (!paramsBinding.referenced) {
-          paramsList.pop()
+          paramsList.pop();
         } else {
-          break
+
+          break;
         }
       }
     }
-    return paramsList
+    return paramsList;
   }
 
   traverse(ast, {
-    VariableDeclaration(path) { // 变量
+    VariableDeclaration(path) {// 变量
       const { node } = path;
       const { declarations } = node;
       node.declarations = declarations.filter((declaration) => {
@@ -75,7 +78,7 @@ module.exports = function () {
           // 通过filter移除掉没有使用的变量
           id.properties = id.properties.filter((property) => {
             const binding = path.scope.getBinding(property.value.name || property.key.name);
-            if (!binding) return undefined
+            if (!binding) return undefined;
             // referenced 变量是否被引用
             // constantViolations 变量被重新定义的地方
             const { referenced, constantViolations } = binding;
@@ -89,22 +92,22 @@ module.exports = function () {
           });
           // 如果对象中所有变量都没有被应用，则该对象整个移除
           return id.properties.length > 0;
-        } else if (t.isArrayPattern(id)) { // 如果是解构形式的数组定义  例如 react中 useState这样的定义
-          const res = []
-          id.elements.forEach(v => {
+        } else if (t.isArrayPattern(id)) {// 如果是解构形式的数组定义  例如 react中 useState这样的定义
+          const res = [];
+          id.elements.forEach((v) => {
             const binding = path.scope.getBinding(v.name);
             const { referenced } = binding;
             // 数组的 constantViolations 都是为[] 空
             // 这里对没有应用的进行过滤 
             if (referenced) {
-              res.push(v)
+              res.push(v);
             }
-          })
+          });
           id.elements = res; // 重新赋值新的数组
           return res.length;
         } else {
           const binding = path.scope.getBinding(id.name);
-          if (!binding) return undefined
+          if (!binding) return undefined;
           const { referenced, constantViolations } = binding;
           if (!referenced && constantViolations.length > 0) {
             constantViolations.map((violationPath) => {
@@ -115,8 +118,10 @@ module.exports = function () {
         }
       });
 
+
       if (node.declarations.length === 0) {
         path.remove();
+        unusedVariablesFound = true;
       }
     },
 
@@ -127,22 +132,24 @@ module.exports = function () {
 
       node.specifiers = specifiers.filter((specifier) => {
         const { local } = specifier;
-        if (local.name === "React") return true
+        if (local.name === "React") return true;
         const binding = path.scope.getBinding(local.name);
         return !!binding.referenced;
       });
       if (node.specifiers.length === 0) {
         path.remove();
+        unusedVariablesFound = true;
       }
     },
     FunctionDeclaration(path) {
       const { node } = path;
       const { id } = node; // 获取id和参数
-      path.node.params = getParamsList(path) || []
+      path.node.params = getParamsList(path) || [];
 
       const binding = path.scope.getBinding(id.name);
       if (!binding.referenced) {
         path.remove();
+        unusedVariablesFound = true;
       }
     },
     // FunctionDeclaration(path) {
@@ -155,19 +162,25 @@ module.exports = function () {
     //   }
     // },
     // 箭头函数进行处理， 定义的变量赋值箭头时， 箭头函数和变量都会编译
-    ArrowFunctionExpression(path) { //
+    ArrowFunctionExpression(path) {//
       const { node } = path;
-      node.params = getParamsList(path) || []
-    },
-  });
+      node.params = getParamsList(path) || [];
+    } });
 
-  activeTextEditor.edit((editBuilder) => {
-    editBuilder.replace(
-      new vscode.Range( // 来获取所要替换的区域
-        new vscode.Position(0, 0),
-        new vscode.Position(activeTextEditor.document.lineCount + 1, 0)
-      ),
-      generate(ast, { retainLines: true, retainFunctionParens: true }).code
-    );
-  });
+
+  if (unusedVariablesFound) {
+    activeTextEditor.edit((editBuilder) => {
+      editBuilder.replace(
+      new vscode.Range(
+      new vscode.Position(0, 0),
+      new vscode.Position(activeTextEditor.document.lineCount + 1, 0)),
+
+      generate(ast, { retainLines: true, retainFunctionParens: true }).code);
+
+
+    });
+    vscode.window.showInformationMessage('Deleted unused variables!');
+  } else {
+    vscode.window.showInformationMessage("No unused variable found!");
+  }
 };
